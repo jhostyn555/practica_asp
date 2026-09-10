c84383fefd7db18c8c540213d2c4dc33c1a19b39
jhostyn555 <jhostynrodrigovargasnavia@gmail.com>
2026-09-10 14:41:44 -0400

    Eliminar comando cdif accidental en Program.cs

--- Program.cs
+++ Program.cs
@@ -52,7 +52,7 @@ using (var scope = app.Services.CreateScope())
     string[] roles = { "Admin", "User" };
     foreach (var role in roles)
     {
-        cdif(!await roleManager.RoleExistsAsync(role));
+        if(!await roleManager.RoleExistsAsync(role));
             await roleManager.CreateAsync(new IdentityRole(role));
     }
 
