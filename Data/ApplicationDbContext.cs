b8c32f4941018b71828e349d1b4567998b1d9463
jhostyn555 <jhostynrodrigovargasnavia@gmail.com>
2026-09-10 10:50:32 -0400

    Primer commit: proyecto ASP.NET

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using proyecto_asp.Models;
+
+namespace proyecto_asp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Configuraciones adicionales si fueran necesarias
+        }
+    }
+}
